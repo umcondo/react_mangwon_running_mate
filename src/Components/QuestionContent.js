dfadac43cd6c7f9ffe7620e4af13ee8963853a61
umcondo <superpowerkdg@gmail.com>
2022-05-18 13:43:05 +0900

    질문페이지 질문제목, 내용 데이터바인딩

--- src/Components/QuestionContent.js
+++ src/Components/QuestionContent.js
@@ -12,18 +12,33 @@ const QuestionContent = ({
 
   const resultInput = useRef();
 
+  // 데이터 조작
+  const [curAnswer1Text1, curAnswer1Text2] = curAnswer1.split("<br>");
+  const [curAnswer2Text1, curAnswer2Text2] = curAnswer2.split("<br>");
+
+  const [titleText1, titleText2, titleText3] =
+    curTitle.split(/<span>|<\/span>/g);
+
   return (
     <div className="question_content">
       <img className="question_img" src={env.PUBLIC_URL + curImg} alt="" />
       <div className="question_head">
-        <h1>{curTitle}</h1>
+        <h1>
+          {titleText1}
+          <span>{titleText2}</span>
+          {titleText3}
+        </h1>
       </div>
       <div className="question_text_box">
         <div className="question_text" onClick={nextQuestion} ref={resultInput}>
-          {curAnswer1}
+          {curAnswer1Text1}
+          <br />
+          {curAnswer1Text2}
         </div>
         <div className="question_text" onClick={nextQuestion} ref={resultInput}>
-          {curAnswer2}
+          {curAnswer2Text1}
+          <br />
+          {curAnswer2Text2}
         </div>
       </div>
     </div>
